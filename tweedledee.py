c36ede0a5c533cb3d13504bba0425f1e334e20c3
Iqbal Hadi Subekti <33026916+IqbalHadiSubekti@users.noreply.github.com>
2021-11-18 12:02:00 +0700

    tweedledee
    cek

---
+++ tweedledee.py
@@ -0,0 +1,11 @@
+print("a")
+
+tweedledoom = ("1861-10-23",["Bersyair","Menyanyi","Suka Berpura-pura"])
+print("Tweedledoom: ",tweedledoom)
+tweedledee = ("1861-10-23",["Bersyair","Menyanyi","Suka Berpura-pura"])
+print("Tweedledee: ",tweedledee)
+
+print("b")
+
+tweedledee[1][0] = "Memanah"
+print("Tweedledee: ", tweedledee)
